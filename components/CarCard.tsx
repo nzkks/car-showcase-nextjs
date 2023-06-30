410d12edfd7a571cdb4bb902e0d3d3852af9686e
Shanthosh Krishnakumar <kkshanthosh@gmail.com>
2023-07-01 11:09:41 +1200

    show the fetched car details (basic)

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -1,7 +1,42 @@
-import React from 'react';
+'use client';
 
-const CarCard = () => {
-  return <div>CarCard</div>;
+import { useState } from 'react';
+import Image from 'next/image';
+import { CarProps } from '@/types';
+
+interface CarCardProps {
+  car: CarProps;
+}
+
+const CarCard = ({ car }: CarCardProps) => {
+  const {
+    city_mpg,
+    class: carClass,
+    combination_mpg,
+    cylinders,
+    displacement,
+    drive,
+    fuel_type,
+    highway_mpg,
+    make,
+    model,
+    transmission,
+    year
+  } = car;
+
+  return (
+    <div className="car-card group">
+      <div className="car-card__content">
+        <h2 className="car-card__content-title">
+          {make} {model}
+        </h2>
+      </div>
+
+      <p>
+        <span>Car Rent...</span>
+      </p>
+    </div>
+  );
 };
 
 export default CarCard;
