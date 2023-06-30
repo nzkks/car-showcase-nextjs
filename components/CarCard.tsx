a7717e7c247bcfdc29dc193f8616f5fa64b40ea5
Shanthosh Krishnakumar <kkshanthosh@gmail.com>
2023-07-01 11:52:35 +1200

    CarCard: Show few more car details

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -4,12 +4,15 @@ import { useState } from 'react';
 import Image from 'next/image';
 import { CarProps } from '@/types';
 import { calculateCarRent } from '@/utils';
+import CustomButton from './CustomButton';
 
 interface CarCardProps {
   car: CarProps;
 }
 
 const CarCard = ({ car }: CarCardProps) => {
+  const [open, setOpen] = useState(false);
+
   const {
     city_mpg,
     class: carClass,
@@ -40,6 +43,37 @@ const CarCard = ({ car }: CarCardProps) => {
         {carRent}
         <span className="self-end text-[14px] font-medium">/day</span>
       </p>
+
+      <div className="relative w-full h-40 my-3 object-contain">
+        <Image src="/hero.png" alt="car model" fill priority className="object-contain" />
+      </div>
+
+      <div className="relative flex w-full mt-2">
+        <div className="flex group-hover:invisible w-full justify-between text-grey">
+          <div className="flex flex-col justify-center items-center gap-2">
+            <Image src="/steering-wheel.svg" width={20} height={20} alt="steering wheel" />
+            <p className="text-[14px]">{transmission === 'a' ? 'Automatic' : 'manual'}</p>
+          </div>
+          <div className="flex flex-col justify-center items-center gap-2">
+            <Image src="/tire.svg" width={20} height={20} alt="steering wheel" />
+            <p className="text-[14px]">{drive.toUpperCase()}</p>
+          </div>
+          <div className="flex flex-col justify-center items-center gap-2">
+            <Image src="/gas.svg" width={20} height={20} alt="steering wheel" />
+            <p className="text-[14px]">{city_mpg} MPG</p>
+          </div>
+        </div>
+
+        <div className="car-card__btn-container">
+          <CustomButton
+            title="View More"
+            containerStyles="w-full py-[16px] rounded-full bg-primary-blue"
+            textStyles="text-white text-[14px] leading-[17px] font-bold"
+            rightIcon="/right-arrow.svg"
+            handleClick={() => setOpen(true)}
+          />
+        </div>
+      </div>
     </div>
   );
 };
