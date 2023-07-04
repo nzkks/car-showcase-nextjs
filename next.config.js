88b99cc9b2b2765ebc18015871f99388175a7c02
Shanthosh Krishnakumar <kkshanthosh@gmail.com>
2023-07-04 19:53:24 +1200

    minor fix

--- next.config.js
+++ next.config.js
@@ -1,8 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  env: {
-    X_RAPIDAPI_KEY: '3aa5476c86msh6971a4af39d4004p122366jsn78d617f75c3c'
-  }
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
